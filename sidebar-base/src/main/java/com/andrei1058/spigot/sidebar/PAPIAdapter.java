52905e22b09ebb16db5c9f6684b34c4d7493242d
Tom Keuper <tomkeuper@live.nl>
2023-03-23 20:12:46 +0100

    Fix papi not formatting on first join

--- sidebar-base/src/main/java/com/andrei1058/spigot/sidebar/PAPIAdapter.java
+++ sidebar-base/src/main/java/com/andrei1058/spigot/sidebar/PAPIAdapter.java
@@ -1,8 +1,12 @@
 package com.andrei1058.spigot.sidebar;
 
 import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class PAPIAdapter implements PAPISupport {
     @Override
@@ -12,11 +16,8 @@ class PAPIAdapter implements PAPISupport {
 
     @Override
     public boolean hasPlaceholders(@NotNull String s) {
-        for (String sub : s.split(" ")){
-            if (sub.matches(PlaceholderAPI.getPlaceholderPattern().pattern())){
-                return true;
-            }
-        }
-        return false;
+        Pattern pattern =  Pattern.compile("[%]([^%]+)[%]");
+        Matcher matcher = pattern.matcher(s);
+        return matcher.find();
     }
 }
