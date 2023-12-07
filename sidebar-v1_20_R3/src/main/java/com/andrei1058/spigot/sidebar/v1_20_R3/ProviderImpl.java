19c65371b94d4e4456ec33bc1f8c4af7017e7468
andrei1058 <andrei7dascalu@gmail.com>
2023-12-07 01:58:06 +0100

    add 1.20.3 support

---
+++ sidebar-v1_20_R3/src/main/java/com/andrei1058/spigot/sidebar/v1_20_R3/ProviderImpl.java
@@ -0,0 +1,67 @@
+package com.andrei1058.spigot.sidebar.v1_20_R3;
+
+import com.andrei1058.spigot.sidebar.*;
+import net.minecraft.network.chat.IChatBaseComponent;
+import net.minecraft.network.protocol.game.PacketPlayOutPlayerListHeaderFooter;
+import net.minecraft.network.protocol.game.PacketPlayOutScoreboardScore;
+import net.minecraft.server.network.PlayerConnection;
+import net.minecraft.world.scores.criteria.IScoreboardCriteria;
+import org.bukkit.craftbukkit.v1_20_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+
+@SuppressWarnings("unused")
+public class ProviderImpl extends SidebarProvider {
+
+    private static SidebarProvider instance;
+
+    @Override
+    public Sidebar createSidebar(SidebarLine title, Collection<SidebarLine> lines, Collection<PlaceholderProvider> placeholderProviders) {
+        return new SidebarImpl(title, lines, placeholderProviders);
+    }
+
+    @Override
+    public SidebarObjective createObjective(@NotNull WrappedSidebar sidebar, String name, boolean health, SidebarLine title, int type) {
+        return ((SidebarImpl)sidebar).createObjective(name, health ? IScoreboardCriteria.f : IScoreboardCriteria.a, title, type);
+    }
+
+    @Override
+    public ScoreLine createScoreLine(WrappedSidebar sidebar, SidebarLine line, int score, String color) {
+        return ((SidebarImpl)sidebar).createScore(line, score, color);
+    }
+
+    public void sendScore(@NotNull WrappedSidebar sidebar, String playerName, int score) {
+        if (sidebar.getHealthObjective() == null) return;
+        PacketPlayOutScoreboardScore packetPlayOutScoreboardScore = new PacketPlayOutScoreboardScore(
+                sidebar.getHealthObjective().getName(),
+                playerName,
+                score,
+                null,
+                null
+        );
+        for (Player player : sidebar.getReceivers()) {
+            PlayerConnection playerConnection = ((CraftPlayer) player).getHandle().c;
+            playerConnection.b(packetPlayOutScoreboardScore);
+        }
+    }
+
+    @Override
+    public VersionedTabGroup createPlayerTab(WrappedSidebar sidebar, String identifier, SidebarLine prefix, SidebarLine suffix,
+                                             PlayerTab.PushingRule pushingRule, PlayerTab.NameTagVisibility tagVisibility,
+                                             @Nullable Collection<PlaceholderProvider> placeholders) {
+        return new PlayerListImpl(sidebar, identifier, prefix, suffix, pushingRule, tagVisibility, placeholders);
+    }
+
+    @Override
+    public void sendHeaderFooter(Player player, String header, String footer) {
+        PacketPlayOutPlayerListHeaderFooter packet = new PacketPlayOutPlayerListHeaderFooter(IChatBaseComponent.b(header), IChatBaseComponent.b(footer));
+        ((CraftPlayer)player).getHandle().c.b(packet);
+    }
+
+    public static SidebarProvider getInstance() {
+        return null == instance ? instance = new ProviderImpl() : instance;
+    }
+}
